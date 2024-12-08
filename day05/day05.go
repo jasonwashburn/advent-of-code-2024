e8b70bc438337608e93608d5a8ea18c229e46cf0
Jason Washburn <jason.washburn@gmail.com>
2024-12-08 08:21:44 -0600

    feat: Day05 Complete

--- day05/day05.go
+++ day05/day05.go
@@ -30,7 +30,8 @@ func Solve() {
 	// input := utils.ReadInput("./day05/sample.txt")
 	input := utils.ReadInput("./day05/input.txt")
 	mustComeAfters := make(map[int]pageSet)
-	var records []UpdateRecord
+	var updateRecords []UpdateRecord
+	var badUpdateRecords []UpdateRecord
 
 	sectionOne := true
 	for _, line := range strings.Split(input, "\n") {
@@ -71,16 +72,14 @@ func Solve() {
 				numSlice = append(numSlice, num)
 			}
 
-			fmt.Println(numSlice)
-			records = append(records, UpdateRecord{pages: numSlice})
+			updateRecords = append(updateRecords, UpdateRecord{pages: numSlice})
 		}
 	}
-	fmt.Println(records)
 
 	// Go through records
 
 	returnTotal := 0
-	for _, record := range records {
+	for _, record := range updateRecords {
 		goodRecord := true
 		pages := record.pages
 		for currentPageIndex, currentPage := range pages {
@@ -94,23 +93,64 @@ func Solve() {
 					break
 				}
 
-				if !goodRecord {
-					// found a bad page, no reason to continue through the rest for this
-					// record
-					break
-				}
+			}
+			if !goodRecord {
+				// found a bad page, no reason to continue through the rest for this
+				// record
+				//
+				// add record to list of bad records for processing in part 2
+				break
 			}
 		}
 		if goodRecord {
 			// This record is in order
-			fmt.Println(pages)
 			middleIndex := len(pages) / 2
 			middlePageNum := pages[middleIndex]
-			fmt.Println("Middle number is: ", middlePageNum)
 			returnTotal += middlePageNum
+		} else {
+			badUpdateRecords = append(badUpdateRecords, record)
 		}
 	}
 	fmt.Println("Part 1 Total is : ", returnTotal)
+
+	fmt.Println("Part 2...........")
+
+	badMiddleTotal := 0
+	for _, badRecord := range badUpdateRecords {
+		reorderedPages := reorderPages(badRecord.pages, mustComeAfters)
+		middleNum := reorderedPages[len(reorderedPages)/2]
+		badMiddleTotal += middleNum
+	}
+	fmt.Println("Part 2 Total: ", badMiddleTotal)
+}
+
+func reorderPages(pages []int, mustComeAfters map[int]pageSet) []int {
+	outOfOrder := true
+	for outOfOrder {
+		goodRecord := true
+		for currentPageIndex, currentPage := range pages {
+			for laterPageIndex, laterPage := range pages[currentPageIndex+1:] {
+				afterRules := mustComeAfters[laterPage]
+				// if currentPage is in afterRules for later pages, it violates order
+				// because it is currently before the later page under test
+				if afterRules.contains(currentPage) {
+					// Out of order so we must make a swap
+					pages[currentPageIndex] = laterPage
+					pages[currentPageIndex+1+laterPageIndex] = currentPage
+					goodRecord = false
+					break
+				}
+			}
+			if !goodRecord {
+				break
+			}
+		}
+		if goodRecord {
+			outOfOrder = false
+		}
+	}
+
+	return pages
 }
 
 func processPairLine(pair string) []int {
